9bf561348ddd7ee6d8fbdd865c53109e3ff63a06
olgafedorec <olga_fedorec@yahoo.com>
2024-04-26 11:56:29 +0300

    creating gallery

---
+++ src/js/1-gallery.js
@@ -0,0 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
---
+++ src/js/gallery-items.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      '../img/g-01-optimized.jpg',
+    original:
+      '../img/g-01-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-02-optimized.jpg',
+    original:
+      '../img/g-02-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-03-optimized.jpg',
+    original:
+      '../img/g-03-optimized.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      '../img/g-04-optimized.jpg',
+    original:
+      '../img/g-04-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-05-optimized.jpg',
+    original:
+      '../img/g-05-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-06-optimized.jpg',
+    original:
+      '../img/g-06-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-07-optimized.jpg',
+    original:
+      '../img/g-07-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-08-optimized.jpg',
+    original:
+      '../img/g-08-optimized.jpg',
+    description: '',
+  },
+  {
+    preview:
+      '../img/g-09-optimized.jpg',
+    original:
+      '../img/g-09-optimized.jpg',
+    description: '',
+  },
+];
